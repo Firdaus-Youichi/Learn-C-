bf22087987de334f90b076c84f6401329cd6d031
unknown <firdayosi17@gmail.com>
2023-03-31 23:18:47 +0700

    Data Type

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 123456789;
+    float b = 3.14;
+    double c = 3.99;
+    string d = "One, Two, Three";
+    char e = 'X';
+    bool f = false;
+    bool g = true;
+
+    cout << a << " = Interger" << endl;
+    cout << b << " = Floating" << endl;
+    cout << c << " = Floating but Double" << endl;
+    cout << d << " = String" << endl;
+    cout << e << " = Character" << endl;
+    cout << f << " = False Boolean" << endl;
+    cout << g << " = True Boolean" << endl;
+    return 0;
+}
